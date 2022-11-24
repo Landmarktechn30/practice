2d532d4b8bf9758eab34727bc3644819b98189bb
simonelle <97528755+simonelle@users.noreply.github.com>
2022-11-24 06:43:54 -0500

    Update app.java

--- app.java
+++ app.java
@@ -2,3 +2,5 @@ This is code for a web application
 please do not hard code
 make use of variables and dynamic variables
 author: simon mosaso
+This my app.java file
+lets track the changes on our local machine
