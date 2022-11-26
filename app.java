7b5cf4df8fb97b98f3c7fcf9b2c228b730537599
Simeone Monae <mosasosimon@yahoo.com>
2022-11-26 22:56:01 +0000

    Lets correct the wrongs

--- app.java
+++
@@ -1,8 +0,0 @@
-This is a development tool
-then we will commit to the staging and master branch
-we deal with three branches
-rem: the options status, log, show, add, commit, and push and pull
-deployment in progress
-deployment5 master
-deployment4
-delpoy to staging envirnmemt
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+This is the lastest deployment
+deployment in progress
