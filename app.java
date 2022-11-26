0a6e6ba82691b9dbd404ceaa59e2825120dab8ca
Simeone Monae <mosasosimon@yahoo.com>
2022-11-26 16:33:30 +0000

    lines of development

--- app.java
+++ app.java
@@ -2,3 +2,4 @@ This is a development tool
 then we will commit to the staging and master branch
 we deal with three branches
 rem: the options status, log, show, add, commit, and push and pull
+deployment in progress
