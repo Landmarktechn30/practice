8637954ffde416b43282196dc62f705c5b0fbf7b
Simeone Monae <mosasosimon@yahoo.com>
2022-11-26 16:41:35 +0000

    master deployment

--- app.java
+++ app.java
@@ -3,3 +3,4 @@ then we will commit to the staging and master branch
 we deal with three branches
 rem: the options status, log, show, add, commit, and push and pull
 deployment in progress
+deployment5 master
