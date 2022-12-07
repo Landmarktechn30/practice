1c1a70f41722a178782279a7b430b2867a22283f
Simeone Monae <mosasosimon@yahoo.com>
2022-12-07 04:04:14 +0000

    content added to empty file

---
+++ app.py
@@ -0,0 +1,4 @@
+Red white and blue, im proud to wear the name, red white and blue the place that i call home
+when it gets to memphis there friendly people
+but in new orleans you just got the blues
+but no mattter where you go theres one thing everybody knows if you got a bone to pick with us you  gonna lose
