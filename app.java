8ac54d6c40706ef252f11651271f4ea9ab203d94
Simeone Monae <mosasosimon@yahoo.com>
2022-12-07 03:41:07 +0000

    tested clone files

--- app.java
+++ app.java
@@ -4,3 +4,5 @@ make use of variables and dynamic variables
 author: simon mosaso
 This my app.java file
 lets track the changes on our local machine
+We are going to see how cloning works
+also checking how merge, pull and push works
